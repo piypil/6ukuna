dd0625e5552d175695e699b7466f5135fff5287c
piypil <iiawa2000@yandex.ru>
2023-05-18 17:16:58 +0300

    page

--- CRLF/CRLF/vuln/views.py
+++ CRLF/CRLF/vuln/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def vuln_atacker(requset, *args, **kwargs):
-    return HttpResponse('<p>THIS IS ATACKER<p>')
+    return render(requset, 'vulnTemplates/vulnPage.html', {})
+  
