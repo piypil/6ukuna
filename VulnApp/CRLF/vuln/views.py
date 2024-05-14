fcb64997d7b4a45e90bf1d222a210792d4cd0acb
piypil <iiawa2000@yandex.ru>
2024-05-14 22:06:26 +0300

    fix

--- VulnApp/CRLF/vuln/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def vuln_atacker(requset, *args, **kwargs):
-    return render(requset, 'vulnTemplates/vulnPage.html', {})
--- VulnApp/SSRF/vuln/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-def vuln_atacker(request):
-    ip  =  request.META.get('REMOTE_ADDR')
-    return render(request, 'ssrf.html', {'ip_address': ip})
